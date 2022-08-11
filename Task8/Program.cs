b7cf7a02fd27c3ea9789b893c61a85b8cdc3bf71
Ural <kilmyhametov@mail.ru>
2022-08-11 17:23:03 +0500

    Task 8 DONE

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N = {N}");
+int count = 0;
+int a = 0;
+while (count < N)
+{
+    a = count + 1;
+    if (a % 2 == 0)
+    Console.WriteLine(a);
+    count ++;
+    
+}
+
