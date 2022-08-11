f91fb2617d9fb1c111718ddd1acf8cf419052256
Ural <kilmyhametov@mail.ru>
2022-08-11 15:39:13 +0500

    Change converting string

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите первое число");
-int a = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine($"a = { a }" );
 Console.WriteLine("Введите второе число");
-int b = int.Parse(Console.ReadLine());
+int b = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine($"b = { b }" );
 if (a>b)
 {
