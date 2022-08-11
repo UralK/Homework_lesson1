f7908f55e4136e6db832f092bc1723581da4d5ec
Ural <kilmyhametov@mail.ru>
2022-08-11 16:09:53 +0500

    Task6 DONE

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt16(Console.ReadLine());
+if (a % 2 == 0)
+{ 
+    Console.WriteLine("Число четное!");
+}
+else 
+{
+    Console.WriteLine("Число нечетное!");
+}
+
+
+
