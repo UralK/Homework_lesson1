6b88ebd0fbf166b3380cc5d9bae0d81b0e49ece0
Ural <kilmyhametov@mail.ru>
2022-08-11 15:41:53 +0500

    Task4 DONE

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"a = {a}");
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"b = {b}");
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"c = {c}");
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"max = {max}");
